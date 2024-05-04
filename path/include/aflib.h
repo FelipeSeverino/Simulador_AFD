febf96f67c489e28941024a26c06c9535176a114
FelipeSeverino <felipe.carlos.2027@gmail.com>
2024-05-04 16:11:17 -0300

    finalizando aflib

--- path/include/aflib.h
+++ path/include/aflib.h
@@ -1,12 +1,26 @@
 #ifndef AFLIB_H
 #define AFLIB_H
 
-typedef struct estado {
+typedef struct transicao {
+    char symbol;
+    char q_from[5];
+    char q_to[5];
 
+    TRANSICAO next;
+} TRANSICAO;
+
+
+typedef struct estado {
+    int final; // 0 = false, 1 = true
+    char nome[5];
+
+    ESTADO next;
 } ESTADO;
 
 typedef struct af {
-    
+    char estado_inicial[5];
+    ESTADO estado;
+    TRANSICAO transicao;
 } AF;
 
 #endif
